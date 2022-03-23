5c28fa14506ca06c324254d19a5e4a65ef787418
Ben Alderson <benalderson725@gmail.com>
2022-03-22 20:26:51 -0400

    Second GUI Page

--- src/main.py
+++ src/main.py
@@ -4,14 +4,25 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.spinner import Spinner
 from kivy.uix.widget import Widget
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
 
-class Screen(Widget):
+class MatchScreen (Screen):
     pass
 
-class MyApp (App):
+class PostmatchScreen (Screen):
+    pass
+
+class ScreenManager (ScreenManager):
+    pass
+
+kv = Builder.load_file("MatchScreen.kv")
+
+class MainWindow (App):
     def build(self):
-        return Screen()
+        return kv
 
 if __name__ == "__main__":
-    MyApp().run()
+    MainWindow().run()
