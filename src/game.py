c19a3411382eb8e6aed9107f8ddeb664c165dc8c
Ben Alderson <benalderson725@gmail.com>
2022-03-23 08:39:30 -0400

    Goal buttons

---
+++ src/game.py
@@ -0,0 +1,6 @@
+class Game:
+    def __init__(self):
+        self.autonHigh = 0
+        self.autonLow = 0
+        self.teleopHigh = 0
+        self.teleopLow = 0
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from game import Game
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
@@ -7,11 +8,52 @@ from kivy.uix.button import Button
 from kivy.uix.spinner import Spinner
 from kivy.uix.widget import Widget
 from kivy.lang.builder import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
 
-class MatchScreen (Screen):
-    pass
+game = Game()
 
+class MatchScreen (Screen):
+    autonHighLbl = ObjectProperty(None)
+    autonLowLbl = ObjectProperty(None)
+    teleopHighLbl = ObjectProperty(None)
+    teleopLowLbl = ObjectProperty(None)
+
+    def addGoal(self, goal):
+        if goal == "ah":
+            game.autonHigh += 1
+            self.autonHighLbl.text = str(game.autonHigh)
+        elif goal == "al":
+            game.autonLow += 1
+            self.autonLowLbl.text = str(game.autonLow)
+        elif goal == "th":
+            game.teleopHigh += 1
+            self.teleopHighLbl.text = str(game.teleopHigh)
+        elif goal == "tl":
+            game.teleopLow += 1
+            self.teleopLowLbl.text = str(game.teleopLow)
+
+    def removeGoal(self, goal):
+        if goal == "ah":
+            game.autonHigh -= 1
+            if game.autonHigh < 0:
+                game.autonHigh = 0
+            self.autonHighLbl.text = str(game.autonHigh)
+        elif goal == "al":
+            game.autonLow -= 1
+            if game.autonLow < 0:
+                game.autonLow = 0
+            self.autonLowLbl.text = str(game.autonLow)
+        elif goal == "th":
+            game.teleopHigh -= 1
+            if game.teleopHigh < 0:
+                game.teleopHigh = 0
+            self.teleopHighLbl.text = str(game.teleopHigh)
+        elif goal == "tl":
+            game.teleopLow -= 1
+            if game.teleopLow < 0:
+                game.teleopLow = 0
+            self.teleopLowLbl.text = str(game.teleopLow)
 class PostmatchScreen (Screen):
     pass
 
